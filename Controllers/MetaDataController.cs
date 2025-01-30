0497a70fb6415231fe50ce6c32f3557b49ba6e84
Alfred Odey <alfredmodey@gmail.com>
2025-01-30 20:12:47 +0100

    First commit of all necessary files to the repo

---
+++ Controllers/MetaDataController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TheDriverDevHNGAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MetaDataController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetData()
+        {
+            var response = new
+            {
+                Email = "alfredmodey@gmail.com",
+                CurrentDate = DateTime.UtcNow.ToString("o"),
+                GitHubURL = ""
+            };
+
+            return Ok(response);
+        }
+    }
+}
---
+++ MetaData.cs
@@ -0,0 +1,9 @@
+﻿namespace TheDriverDevHNGAPI
+{
+    public class MetaData
+    {
+        public string? Email { get; set; }
+        public DateTime CurrentDate { get; set; }
+        public string? GitHubURL { get; set; }
+    }
+}
