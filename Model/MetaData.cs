db86b256c9f7d29db75b9959dbb201cc5dcb0ef0
Alfred Odey <alfredmodey@gmail.com>
2025-01-30 21:38:37 +0100

    Moved the MetaData.cs file into the created Model folder

---
+++ Model/MetaData.cs
@@ -0,0 +1,9 @@
+﻿namespace TheDriverDevHNGAPI
+{
+    public class MetaData
+    {
+        public string? Email { get; set; }
+        public DateTime Current_Date { get; set; }
+        public string? Github_URL { get; set; }
+    }
+}
