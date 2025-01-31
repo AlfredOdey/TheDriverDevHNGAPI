2687be255061fed5f1f0422133609d72577830ad
Alfred Mamman Odey <154962749+AlfredOdey@users.noreply.github.com>
2025-01-31 03:46:51 +0100

    Delete duplicate MetaData.cs

--- MetaData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TheDriverDevHNGAPI
-{
-    public class MetaData
-    {
-        public string? Email { get; set; }
-        public DateTime Current_Date { get; set; }
-        public string? Github_URL { get; set; }
-    }
-}
